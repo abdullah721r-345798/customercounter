e43b0ed5dfb849134fde2dc5696cd5db93d0f998
abdullah721r-345798 <141143583+abdullah721r-345798@users.noreply.github.com>
2023-08-01 15:10:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
-#Qjlskdjgl
+import torch
+
+
+
+
+# Model
+#model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # or yolov5n - yolov5x6, custom
+
+# Images
+#img = "https://ultralytics.com/images/zidane.jpg"  # or file, Path, PIL, OpenCV, numpy, list
+
+# Inference
+#results = model(img)
+
+# Results
+##results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
