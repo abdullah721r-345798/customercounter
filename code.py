25fb245f269f8719afe2d759a9cac8ecf7eabfc1
abdullah721r-345798 <141143583+abdullah721r-345798@users.noreply.github.com>
2023-08-01 15:38:37 +0300

    Update code.py

--- code.py
+++ code.py
@@ -6,13 +6,13 @@ import torch
 
 
 # Model
-#model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # or yolov5n - yolov5x6, custom
+model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # or yolov5n - yolov5x6, custom
 
 # Images
-#img = "https://ultralytics.com/images/zidane.jpg"  # or file, Path, PIL, OpenCV, numpy, list
+img = "https://ultralytics.com/images/zidane.jpg"  # or file, Path, PIL, OpenCV, numpy, list
 
 # Inference
-#results = model(img)
+results = model(img)
 
 # Results
-##results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
